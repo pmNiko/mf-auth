477df5e60a9c5ea22ca1bc628e29af961795d759
nikodev <martin.panebianco@smandes.gob.ar>
2023-12-07 10:12:36 -0300

    Clean modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ mode }) => {
   Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
@@ -18,7 +17,6 @@ export default defineConfig(({ mode }) => {
         },
         shared: ["react", "react-dom"],
       }),
-      tsconfigPaths(),
     ],
     build: {
       modulePreload: false,
